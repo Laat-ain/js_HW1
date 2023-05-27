a2175dc2ce83b4ca87df3d32a6e7b61d389a8513
Natalia Okhrimenko <ohrim2228n@mail.ru>
2023-05-28 02:00:50 +0300

    added a second task

--- index.js
+++ index.js
@@ -8,35 +8,35 @@ let out_foundation_cost = document.getElementById('out_foundation_cost');
 
 calculation_foundation_cost.onclick = foundation_cost;
 
-function foundation_cost(){
-
+function foundation_cost() {
+	let foundation_cost = Number(foundation_area.value) * Number(depth.value) * Number(cost_per_cubic_meter.value);
+	console.log(foundation_cost);
+	out_foundation_cost.innerText = "Cтоимость фундамента = " + foundation_cost;
 }
 
 /* Вторая задача */
-let board_price = 1500;
-let discount_price;
-let number_purchased_boards = document.getElementById('inp_foundation_area');
+const board_price = 1500;
+let number_purchased_boards = document.getElementById('number_purchased_boards');
 
 let money_calculator = document.getElementById('money_calculator');
 let out_money_calculator = document.getElementById('out_money_calculator');
 
-calculation_foundation_cost.onclick = foundation_cost;
-out.innerText = discount_price;
+money_calculator.onclick = f_money_calculator;
 
-function money_calculator(){
-/* 	если купите 100+ досок то скидка 5%
-если 200+ скидка 10%
-если 300+ скидка 20% */
-
-if(number_purchased_boards => 100 && number_purchased_boards < 200){
-	discount_price = 
-}
-else if(number_purchased_boards => 200 && number_purchased_boards < 300){
-
-}
-else if(number_purchased_boards => 300){
-	
-}
-
-return discount_price;
-}
+function f_money_calculator() {
+	let number_boards = Number(number_purchased_boards.value);
+	let discount_price;
+	if (number_boards >= 100 && number_boards < 200) {
+		discount_price = (board_price * number_boards) - (board_price * number_boards * 0.05);
+	}
+	else if (number_boards >= 200 && number_boards < 300) {
+		discount_price = (board_price * number_boards) - (board_price * number_boards * 0.1);
+	}
+	else if (number_boards >= 300) {
+		discount_price = (board_price * number_boards) - (board_price * number_boards * 0.2);
+	}
+	else{
+		discount_price = board_price * number_boards;
+	}
+	out_money_calculator.innerText = "Cтоимость покупки с учетом скидки  = " + discount_price;
+} 
